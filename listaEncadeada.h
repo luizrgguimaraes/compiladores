c14320fcccdd34ca1309d11574682c136d0d5134
Luiz Reginaldo G Guimaraes <luizreginaldogg@hotmail.com>
2020-04-06 15:12:31 -0300

    lista Encadeada com funcao de criacao apenas

---
+++ listaEncadeada.h
@@ -0,0 +1,7 @@
+#ifndef __LISTAENCADEADA_H__
+#define __LISTAENCADEADA_H__
+
+
+No *novoInteiro(int i);
+No *novoReal(double r);
+No *novaString(char *s);
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+#include "listaEncadeada.h"
+
+int main(){
+	No p =  novoInteiro(20);
+
+}
